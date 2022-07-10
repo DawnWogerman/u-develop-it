bd0c08e044167e202e5db0641ed148cd04fd58a5
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-07-09 20:49:10 -0600

    added db.connect

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-
-
-
-
-
-
 //default respone for any other request (Not Found)
 app.use((req, res) =>{
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Sever running on port ${PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Sever running on port ${PORT}`);
+// });
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
